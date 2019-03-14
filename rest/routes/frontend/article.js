08e076e26210afa1d893147b5c5529be5b20801a
webyangpei <1208812362@qq.com>
2019-03-14 17:20:57 +0800

    优化路由设计

---
+++ rest/routes/frontend/article.js
@@ -0,0 +1,11 @@
+// 文章相关api
+// .get('/api/article/get_detail',ArticleController.get_detail)                         // 获取文章详情
+// .get('/api/article/get_category',ArticleController.get_category)                     // 获取文章类别
+// .get('/api/article/getmenu_category',ArticleController.getmenu_category)             // 获取菜单列表
+// // .post('/api/article/create_upload', upload.alioss, ArticleController.create_upload)  // 上传文章封面图
+// .post('/api/article/create',ArticleController.create)                                // 发表文章
+// .post('/api/article/create_comment',ArticleController.create_comment)                // 发表评论
+// .put('/api/article/article_put',ArticleController.article_put)                       // 编辑文章
+// .put('/api/article/article_praise',ArticleController.article_praise)                 // 点赞文章
+// .delete('/api/article/del_article',ArticleController.del_article)                    // 删除文章
+// .delete('/api/article/del_comment',ArticleController.del_comment)                    // 删除评论
---
+++ rest/routes/frontend/home.js
@@ -0,0 +1,15 @@
+const router = require('koa-router')();
+// const upload = require('../middlewares/upload');
+const { HomeController } = require('../controllers/frontendRouter/index');
+
+// 前端sinn Api
+const routers = router
+	.get('/', HomeController.query)
+	.get('/api/query', HomeController.query)
+
+module.exports = routers;
+
+
+
+
+
