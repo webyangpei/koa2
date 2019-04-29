641520b2b6be6344b57378a2020ac45dba0c299d
webyangpei <1208812362@qq.com>
2019-04-29 15:03:59 +0800

    【修正】mongodb 端口

--- config/common.js
+++ config/common.js
@@ -5,14 +5,14 @@ module.exports = {
     // 开发环境配置
     development: {
         mongo: {
-            uri: 'mongodb://localhost:27017/admin'
+            uri: 'mongodb://localhost:27107/admin'
         },
         port: 8080
     },
     // 生产环境配置
     production: {
         mongo: {
-            uri: 'mongodb://39.105.65.92.:27017/myBlog'
+            uri: 'mongodb://39.105.65.92.:27107/myBlog'
         },
         port: 8080
     }
