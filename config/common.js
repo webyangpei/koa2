b8bbd6d7481f67ab7ae1ccf3824d5d0ecdde974b
webyangpei <1208812362@qq.com>
2020-08-29 17:18:44 +0800

    init

--- config/common.js
+++ config/common.js
@@ -7,12 +7,24 @@ module.exports = {
         mongo: {
             uri: 'mongodb://localhost:27017/admin'
         },
+        mysql: {
+          host: 'localhost',
+          user: 'test',
+          password: '123456',
+          database: 'test'
+        },
         port: 8080
     },
     // 生产环境配置
     production: {
         mongo: {
             uri: 'mongodb://39.105.65.92.:27107/myBlog'
+        },
+        mysql: {
+          host: 'localhost',
+          user: 'test',
+          password: '123456',
+          database: 'test'
         },
         port: 8080
     }
--- rest/models/db.js
+++ rest/models/db.js
@@ -2,24 +2,35 @@
  * use 数据库连接
  * @type {*|Mongoose}
  */
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+// const config = require('../../config/common');
+//
+// const dbConfig = config[process.env.NODE_ENV || 'development'];
+//
+// mongoose.connect(dbConfig.mongo.uri, { useNewUrlParser: true })
+//
+// // 连接成功
+// mongoose.connection.on('connected',  () => {
+//     console.log('Mongoose connection open to ' + dbConfig.mongo.uri);
+// });
+//
+// // 连接失败
+// mongoose.connection.on('connected',  err => {
+//     console.log('Mongoose connection error ' + err);
+// });
+//
+// // 断开链接
+// mongoose.connection.on('disconnected', () => {
+//     console.log('Mongoose connection disconnected');
+// })
+
+/**
+ * use 数据库连接
+ * @type {*|mysql}
+ */
+
+const mysql = require('mysql');
 const config = require('../../config/common');
+const connection = mysql.createConnection(config.development.mysql);
 
-const dbConfig = config[process.env.NODE_ENV || 'development'];
-
-mongoose.connect(dbConfig.mongo.uri, { useNewUrlParser: true })
-
-// 连接成功
-mongoose.connection.on('connected',  () => {
-    console.log('Mongoose connection open to ' + dbConfig.mongo.uri);
-});
-
-// 连接失败
-mongoose.connection.on('connected',  err => {
-    console.log('Mongoose connection error ' + err);
-});
-
-// 断开链接
-mongoose.connection.on('disconnected', () => {
-    console.log('Mongoose connection disconnected');
-})
+connection.connect();
