a1d00e30c48df9a76cce2ae373e2a1a14bca01a6
wb_yangpei <wb_yangpei@kuaishou.com>
2021-01-19 11:23:57 +0800

    feat: user Schema增加userid，去除版本号设置

--- src/models/user/user.model.js
+++ src/models/user/user.model.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+	userid: mongoose.Types.ObjectId(),
     username: { type: String, required: true }, // 用户名
     nickName: { type: String, required: true }, // 昵称
     email: { type: String, default: '' }, // 邮箱
@@ -10,6 +11,6 @@ const UserSchema = new Schema({
     password: { type: String, required: true }, // 密码
     createdAt: { type: Date, default: Date.now }, // 创建时间
     updatedAt: { type: Date, default: Date.now } //修改时间
-})
+}, { versionKey: false })
 
 module.exports = mongoose.model('User', UserSchema);
