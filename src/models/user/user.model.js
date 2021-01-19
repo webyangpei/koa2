d6576b7555d9ba56914b2367b6b264d8b250bb86
wb_yangpei <wb_yangpei@kuaishou.com>
2021-01-19 14:34:05 +0800

    feat(src/models/user/user.model.js): user 集合增加唯一标记 userId

--- src/models/user/user.model.js
+++ src/models/user/user.model.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const ObjectId = mongoose.Types.ObjectId;
+
 const UserSchema = new Schema({
-	userid: mongoose.Types.ObjectId,
-    username: { type: String, required: true }, // 用户名
+	userId: new ObjectId,
+    userName: { type: String, required: true }, // 用户名
     nickName: { type: String, required: true }, // 昵称
     roleId: { type: Number, required: false, default: 0 }, // 角色Id - 默认0为超级管理员
     email: { type: String, default: '' }, // 邮箱
     avatar: { type: String, default: '' }, // 头像
     profile: { type: String,default:'' },   // 个人简介
-    password: { type: String, required: true }, // 密码
+    passWord: { type: String, required: true }, // 密码
     createdAt: { type: Date, default: Date.now }, // 创建时间
     updatedAt: { type: Date, default: Date.now } //修改时间
 }, { versionKey: false })
