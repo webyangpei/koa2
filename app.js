5beafbf79e7096cf736ca0811533d7d79e254679
wb_yangpei <wb_yangpei@kuaishou.com>
2021-01-19 16:32:55 +0800

    chore(app.js): 安装事件监听器 - git push

--- app.js
+++ app.js
@@ -10,9 +10,13 @@ const jwt = require('jsonwebtoken');
 const {connectDB} = require('./src/models/db');
 const {API} = require('./src/index');
 const tokenConfig = { privateKey: 'xxxxxxx' }; // 设置加密私钥
+const createHandler = require('github-webhook-handler');
+const runCommand = require('child_process').spawn;
 
 
 const app = new Koa();
+// 监听远程仓库event事件
+const handler = createHandler({ path: './deploy', secret: 'koa2_123456' });
 
 // 配置session中间件
 app.use(session({
@@ -51,8 +55,23 @@ app.use(API.routes()).use(API.allowedMethods());
 
 // 等待数据库连接成功 然后启动程序
 connectDB().then(() => {
-	app.listen(config.port)
-	console.log('Server is running listen on port: ' + config.port)
+	app.listen(config.port, () => {
+		console.log('Server is running listen on port: ' + config.port)
+	})
+
 })
+
+// 监听报错
+handler.on('error', function (err) {
+	console.error('Error:', err.message)
+});
+
+// 监听到push事件的时候执行我们的自动化脚本
+handler.on('push', function (event) {
+	console.log('Received a push event for %s to %s', event.payload.repository.name, event.payload.ref);
+		runCommand('sh', ['./deploy.sh'], function( txt ){
+			console.log(txt);
+		});
+});
 
 module.exports = app;
