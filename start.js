d73cc42b28df89e4bff78fd644c0b49a3bbe98e1
wb_yangpei <wb_yangpei@kuaishou.com>
2021-06-01 19:37:44 +0800

    feat: 修改babel配置

--- start.js
+++ start.js
@@ -5,5 +5,6 @@ require('babel-register')
 		plugins: ['babel-plugin-transform-es2015-modules-commonjs'],
 	}
 )
-
+require('babel-core')
+require('babel-polyfill')
 module.exports = require('./app.js')
